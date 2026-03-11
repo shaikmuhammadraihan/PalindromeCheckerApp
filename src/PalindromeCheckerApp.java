793dc500f08d6dca394973821c1d68cb214a1a93
shaikmuhammadraihan <ms5216.srmist.edu>
2026-03-11 16:35:32 +0530

    UC2:done

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,3 +1,16 @@
-public class PalindromeCheckerApp{
+public class PalindromeCheckerApp {
+    public static void main(String[] args) {
+        String original = "madam";
+        String reversed = "";
 
+        for (int i = original.length() - 1; i >= 0; i--) {
+            reversed = reversed + original.charAt(i);
+        }
+
+        if (original.equals(reversed)) {
+            System.out.println("The string \"" + original + "\" is a palindrome.");
+        } else {
+            System.out.println("The string \"" + original + "\" is NOT a palindrome.");
+        }
+    }
 }
